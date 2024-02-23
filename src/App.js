1e54c39b6e3bb4b9afc2528711607217935a01d3
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-23 20:05:41 +0530

    feat: added routes and set up app js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Routes, Route } from "react-router";
+import { Navbar } from "./components/Navbar/navbar";
+import { Expenses } from "./pages/Expenses/expenses";
+import { Homepage } from "./pages/Homepage/homepage";
+import { Income } from "./pages/Income/income";
+import { Reports } from "./pages/Reports/reports";
+import { Savings } from "./pages/Savings/savings";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/income" element={<Income />} />
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/savings" element={<Savings />} />
+        <Route path="/reports" element={<Reports />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
