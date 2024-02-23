6abb80b12d96d6faa9c9cff9785b384732ba1b78
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-23 19:57:40 +0530

    feat: created action types

---
+++ src/utils/actionTypes.js
@@ -0,0 +1,7 @@
+export const FETCH_INCOME = "FETCH_INCOME";
+export const FETCH_EXPENSE = "FETCH_EXPENSE";
+export const FETCH_SAVING = "FETCH_SAVING";
+export const SORT_INCOME = "SORT_INCOME";
+export const SORT_EXPENSE = "SORT_EXPENSE";
+export const SORT_SAVING = "SORT_SAVING";
+export const ADD_ENTRY = "ADD_ENTRY";
